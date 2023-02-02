8f8b7934d8f7570078f945f282e82b77adf4fb94
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-02-01 17:45:03 -0800

    minor changes

--- data_get.sh
+++ data_get.sh
@@ -1,16 +1,12 @@
 
 export $(xargs < .env)
 
-# SKIPPED - difficult multiline format
-# [Deidentified Medical Text](https://physionet.org/content/deidentifiedmedicaltext/1.0/)
-# wget -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/deidentifiedmedicaltext/1.0/
-
 # [Phenotype Annotations for Patient Notes in the MIMIC-III Database](https://physionet.org/content/phenotype-annotations-mimic/1.20.03/)
-wget -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/phenotype-annotations-mimic/1.20.03/
+wget -q -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/phenotype-annotations-mimic/1.20.03/
 
 # # [MIMIC-IV-Note: Deidentified free-text clinical notes](https://physionet.org/content/mimic-iv-note/2.2/)
-wget -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/mimic-iv-note/2.2/
+wget -q -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/mimic-iv-note/2.2/
 
 # [MIMIC-IV](https://physionet.org/content/mimiciv/2.2/)
-wget -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/mimiciv/2.2/
+wget -q -r -N -c -np --user ${PHYSIONET_USERNAME} --password ${PHYSIONET_PASSWORD} https://physionet.org/files/mimiciv/2.2/
 
